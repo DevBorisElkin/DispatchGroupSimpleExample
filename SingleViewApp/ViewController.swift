dcf629da4d7b346d289cf01f152de900bc27c9cc
Boris <ea.foreverrr@gmail.com>
2022-05-10 13:05:43 +0300

    Initial Commit

---
+++ SingleViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SingleViewApp
+//
+//  Created by test on 10.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
