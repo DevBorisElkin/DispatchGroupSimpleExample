de569028dbdc25d5e64e371ee4aede7257fbd137
Boris <ea.foreverrr@gmail.com>
2022-05-10 13:28:09 +0300

    Initial commit

--- SingleViewApp/ViewController.swift
+++ SingleViewApp/ViewController.swift
@@ -9,9 +9,36 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let array : [TimeInterval] = [
+        1,
+        3,
+        5,
+        7,
+        2,
+        7
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        getData()
+    }
+    
+    func getData(){
+        let group = DispatchGroup()
+        
+        for number in array{
+            group.enter()
+            print("Entering group with number \(number)")
+            DispatchQueue.global().asyncAfter(deadline: .now() + number, execute: {
+                group.leave()
+                print("leaving group with number \(number)")
+            })
+        }
+        
+        group.notify(queue: .main, execute: {
+            print("Done with all operations")
+            self.view.backgroundColor = .brown
+        })
     }
 
 
